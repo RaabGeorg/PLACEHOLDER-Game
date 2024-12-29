6b43d69137c4862c73f4b27cb702056682b44761
RaabGeorg <raab.georg02@gmail.com>
2024-12-29 16:51:27 +0100

    First

--- src/main/java/com/example/placeholdergame/HelloController.java
+++ src/main/java/com/example/placeholdergame/HelloController.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label welcomeText; //testwelcome
 
     @FXML
     protected void onHelloButtonClick() {
